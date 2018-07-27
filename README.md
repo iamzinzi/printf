3030f8a321ef7fa0adef405604f0512ceab2044c
Jinji Zhang <362@holbertonschool.com>
2018-07-27 22:58:04 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 0x10 - C - printf
+---
+## Description
+This project in the Low Level Programming series is about:
+* What are variadic functions
+* How to use va_start, va_arg and va_end macros
+* Understanding how to implement a basic printf function
+
+## Files
+---
+File|Task
+---|---
+holberton.h | header file
+
+## Author
+Jinji Zhang, Robert Glatzel
---
+++ holberton.h
@@ -0,0 +1,24 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * struct format_t - format specifier
+ * @type: format specifier
+ * @func: output function to be used on matching type
+ */
+typedef struct format_t
+{
+	char *type;
+	void (*func)(va_list);
+} format_t;
+
+int _putchar(char c);
+
+#endif /* _HOLBERTON_H_ */
+
+/*
+void print_char(va_list args)
+{
+	write(1, va_arg(args, char), 1);
+}
+*/
