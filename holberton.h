bfb61d17737f57e4867680253b4b808e81a131f6
Jinji Zhang <362@holbertonschool.com>
2018-07-30 23:26:08 +0000

    Functionality to print numbers both positive and negative

--- holberton.h
+++ holberton.h
@@ -16,6 +16,7 @@ typedef struct format_t
 int _printf(char *format, ...);
 int print_char(va_list, char *buf);
 int print_string(va_list ap, char *buf);
+int print_number(va_list ap, char *buf);
 int _putchar(char c);
 int _strlen(char *s);
 int (*get_spec_func(char *s))(va_list ap, char *buf);
