17dafd333aabb9080473e34455bfd48d82494465
Jinji Zhang <362@holbertonschool.com>
2018-07-30 18:02:01 +0000

    Fix for git merge conflicts

--- _printf.c
+++ _printf.c
@@ -1,9 +1,5 @@
 #include <stdarg.h>
-<<<<<<< HEAD
-#include <unistd.h>
-=======
 #include <stdlib.h>
->>>>>>> 57d9c3df9574ac1def5df8d51f72071c6b5fa7cd
 #include "holberton.h"
 
 #define MAXSIZE 1024
@@ -16,24 +12,15 @@ int _printf(char *format, ...)
 
 	va_start(ap, format);
 
-<<<<<<< HEAD
-=======
 	count = 0;
->>>>>>> 57d9c3df9574ac1def5df8d51f72071c6b5fa7cd
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
-<<<<<<< HEAD
 			/* if found '%', move to format specifier */
 			i++;
-			get_spec_func(&format[i])(ap, buf);
-
-=======
-			/* if found, move to format specifier */
-			i++;
 			count += get_spec_func(&format[i])(ap, buf);
->>>>>>> 57d9c3df9574ac1def5df8d51f72071c6b5fa7cd
+
 		}
 		else
 		{
--- holberton.h
+++ holberton.h
@@ -17,13 +17,7 @@ int _printf(char *format, ...);
 int print_char(va_list, char *buf);
 int print_string(va_list ap, char *buf);
 int _putchar(char c);
-<<<<<<< HEAD
-int _printf(char *format, ...);
-int print_char(va_list, char *);
-int (*get_spec_func(char *s))(va_list ap, char *buffer);
-=======
 int _strlen(char *s);
 int (*get_spec_func(char *s))(va_list ap, char *buf);
->>>>>>> 57d9c3df9574ac1def5df8d51f72071c6b5fa7cd
 
 #endif /* _HOLBERTON_H_ */
